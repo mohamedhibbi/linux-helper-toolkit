29a90996c64901080fb5cdf98acac0f8943c08b7
Mohamed Hibbi <medhibbi7@gmail.com>
2026-01-15 16:10:27 +0100

    Update backup_home.sh

--- bash-scripts/backup_home.sh
+++ bash-scripts/backup_home.sh
@@ -2,7 +2,7 @@
 # Backup home folder 
 
 backup_file="home_backup_$(date +%Y%m%d).tar.gz
-tar -cfvz "$backup_file" ~/ 
+tar -czvf "$backup_file" ~/ 
 echo "-------------------------"
 echo "Backup Created: $backup_file" 
 
